0ac575f05c5819fbcbbfdc1498db6c3ffd0f49e7
Arunav Singh <arunavsingh999@gmail.com>
2025-05-30 21:42:04 +0530

    login route setup

--- models/User.js
+++ models/User.js
@@ -12,13 +12,13 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-UserSchema.pre("save", async (next) => {
+UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-UserSchema.methods.comparePassword = async (candidatePassword) => {
+UserSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
